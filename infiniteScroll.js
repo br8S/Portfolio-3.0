077082bead4a4199d10f6acf6491910ebdf6e45f
br8S <56180503+br8S@users.noreply.github.com>
2021-04-02 18:13:52 -0700

    added infinite scroll in both directions for testing

--- infiniteScroll.js
+++ infiniteScroll.js
@@ -1,6 +1,7 @@
 window.addEventListener('scroll', () => {
     if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
         var content_clone = document.querySelector('.content').cloneNode(true);
+        //document.querySelector('.content').before(content_clone);
         document.querySelector('.container').appendChild(content_clone);
     }
 })
@@ -8,6 +9,8 @@ window.addEventListener('scroll', () => {
 window.onload = pageScroll;
 
 function pageScroll() {
+    var content_clone = document.querySelector('.content').cloneNode(true);
+    document.querySelector('.content').before(content_clone);
     window.scrollBy(0,1); // horizontal and vertical scroll increments
     scrolldelay = setTimeout('pageScroll()',10); // scrolls every 100 milliseconds
 }
