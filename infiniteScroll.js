d36335c25aed43f0a8b84ae05035a1e0f7388374
br8S <56180503+br8S@users.noreply.github.com>
2021-04-07 14:11:33 -0700

    removed infinite scroll from mobile devices

--- infiniteScroll.js
+++ infiniteScroll.js
@@ -35,27 +35,30 @@ function pageScroll() {
 }
 
 window.addEventListener('scroll', () => {
-    // when we reach bottom of page we want to clone page after to give illusion of infinite scroll down
-    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
-        var content_clone = document.querySelector('.content').cloneNode(true);
-        document.querySelector('.container').appendChild(content_clone);
 
-        // makes sure we never exceed at most 4 clones of the page
-        if(document.querySelectorAll('.content').length >= 5){
-            var list_of_content = document.querySelectorAll('.content');
-            list_of_content[0].remove();
+    if( window.innerWidth >= 980) {
+        // when we reach bottom of page we want to clone page after to give illusion of infinite scroll down
+        if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+            var content_clone = document.querySelector('.content').cloneNode(true);
+            document.querySelector('.container').appendChild(content_clone);
+
+            // makes sure we never exceed at most 4 clones of the page
+            if(document.querySelectorAll('.content').length >= 5){
+                var list_of_content = document.querySelectorAll('.content');
+                list_of_content[0].remove();
+            }
         }
-    }
 
-    // when we reach top of page we want to clone page before to give illusion of infinite scroll up
-    if(window.scrollY <= 1){
-        var content_clone = document.querySelector('.content').cloneNode(true);
-        document.querySelector('.content').before(content_clone);
+        // when we reach top of page we want to clone page before to give illusion of infinite scroll up
+        if(window.scrollY <= 1){
+            var content_clone = document.querySelector('.content').cloneNode(true);
+            document.querySelector('.content').before(content_clone);
 
-        // makes sure we never exceed at most 4 clones of the page
-        if(document.querySelectorAll('.content').length >= 5){
-            var list_of_content = document.querySelectorAll('.content');
-            list_of_content[4].remove();
+            // makes sure we never exceed at most 4 clones of the page
+            if(document.querySelectorAll('.content').length >= 5){
+                var list_of_content = document.querySelectorAll('.content');
+                list_of_content[4].remove();
+            }
         }
     }
 })
