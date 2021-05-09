445ccbed5e80a2fec7a8b527f856cb1895f16dca
br8S <56180503+br8S@users.noreply.github.com>
2021-05-08 17:37:27 -0700

    added on load animations

---
+++ loader.js
@@ -0,0 +1,62 @@
+window.addEventListener('load', function() {
+    gsap.to(".wrapper", {
+        opacity: 0,
+        ease: Expo.easeInOut,
+        delay: 3.6
+    })
+
+    gsap.from(".cacti-container", {
+        opacity: 0,
+        ease: Expo.easeInOut,
+        delay: 5.6
+    })
+
+    gsap.from(".stussy-container", {
+        opacity: 0,
+        duration: 1.6,
+        ease: Expo.easeInOut,
+        delay: 5.6
+    })
+
+    gsap.from(".ballastpt-container", {
+        opacity: 0,
+        duration: 1.6,
+        ease: Expo.easeInOut,
+        delay: 5.6
+    })
+
+    gsap.from(".br8s-container", {
+        opacity: 0,
+        duration: 1.6,
+        ease: Expo.easeInOut,
+        delay: 5.6
+    })
+
+    gsap.from(".sanchezwedding-container", {
+        opacity: 0,
+        duration: 1.6,
+        ease: Expo.easeInOut,
+        delay: 5.6
+    })
+
+    gsap.from(".mata", {
+        opacity: 0,
+        duration: 1.6,
+        ease: Expo.easeInOut,
+        delay: 7.6
+    })
+
+    gsap.from(".nav-bar", {
+        opacity: 0,
+        duration: 1.6,
+        ease: Expo.easeInOut,
+        delay: 6.4
+    })
+
+    gsap.from(".foot-bar", {
+        opacity: 0,
+        duration: 1.6,
+        ease: Expo.easeInOut,
+        delay: 6.4
+    })
+})
--- projectLinks.js
+++ projectLinks.js
@@ -1,26 +1,24 @@
-window.addEventListener('load', function() {
-    document.querySelector(".cacti").addEventListener('click', () => {
-        console.log("clicked")
-        location.href = 'https://cacti-br8s-dev.herokuapp.com/';
-    })
+document.querySelector(".cacti").addEventListener('click', () => {
+    console.log("clicked")
+    location.href = 'https://cacti-br8s-dev.herokuapp.com/';
+})
 
-    document.querySelector(".stussy").addEventListener('click', () => {
-        console.log("clicked")
-        location.href = 'https://br8s.github.io/Stussy-Landing-Page/';
-    })
+document.querySelector(".stussy").addEventListener('click', () => {
+    console.log("clicked")
+    location.href = 'https://br8s.github.io/Stussy-Landing-Page/';
+})
 
-    document.querySelector(".ballastpt").addEventListener('click', () => {
-        console.log("clicked")
-        location.href = 'https://serene-chandrasekhar-436e23.netlify.app';
-    })
+document.querySelector(".ballastpt").addEventListener('click', () => {
+    console.log("clicked")
+    location.href = 'https://serene-chandrasekhar-436e23.netlify.app';
+})
 
-    document.querySelector(".br8s").addEventListener('click', () => {
-        console.log("clicked")
-        location.href = 'https://br8s-threejs.herokuapp.com/';
-    })
+document.querySelector(".br8s").addEventListener('click', () => {
+    console.log("clicked")
+    location.href = 'https://br8s-threejs.herokuapp.com/';
+})
 
-    document.querySelector(".sanchezwedding").addEventListener('click', () => {
-        console.log("clicked")
-        location.href = 'https://sanchezryndwedding.com/';
-    })
-},false);
+document.querySelector(".sanchezwedding").addEventListener('click', () => {
+    console.log("clicked")
+    location.href = 'https://sanchezryndwedding.com/';
+})
